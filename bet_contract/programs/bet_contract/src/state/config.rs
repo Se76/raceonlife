be4f8749de3737c741d4563a8579682e9b764f54
NickNut <68846529+Se76@users.noreply.github.com>
2025-01-09 10:25:57 +0100

    add two parameters to the config

--- bet_contract/programs/bet_contract/src/state/config.rs
+++ bet_contract/programs/bet_contract/src/state/config.rs
@@ -4,7 +4,9 @@ use anchor_lang::prelude::*;
 #[derive(InitSpace)]
 pub struct Config {
     pub pubkey_initializer: Pubkey,
+    pub rating_initializer: u64,
     pub pubkey_taker: Pubkey,
+    pub rating_taker: u64,
     pub vault: Pubkey,
     pub vault_bump: u8,
     pub winner: Option<Pubkey>,
