661bf821dfd60413367e87778a0539976a382421
NickNut <68846529+Se76@users.noreply.github.com>
2025-01-09 10:55:28 +0100

    add accounts into an instruction

--- bet_contract/programs/bet_contract/src/instructions/initialize.rs
+++ bet_contract/programs/bet_contract/src/instructions/initialize.rs
@@ -1,1 +1,27 @@
 use anchor_lang::prelude::*;
+use crate::state::config::Config;
+
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    #[account(mut)]
+    pub initializer: Signer<'info>,
+    #[account(
+        init,
+        payer = initializer,
+        space = Config::INIT_SPACE + 8,
+        seeds = [b"config", initializer.key().as_ref(), b"bet_raceonlife"],
+        bump
+    )]
+    pub config: Account<'info, Config>,
+    #[account(
+        seeds = [
+                    b"vault", 
+                    initializer.key().as_ref(), 
+                    config.key().as_ref(),
+                    b"bet_raceonlife"
+                ],
+        bump
+    )]
+    pub vault: SystemAccount<'info>,
+    pub system_program: Program<'info, System>,
+}
