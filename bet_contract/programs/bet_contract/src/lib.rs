f849da0e226e47186390fc2f182f04e06341111e
NickNut <68846529+Se76@users.noreply.github.com>
2025-01-10 15:26:41 +0100

    add initialize_bet_with_initializer to lib.rs

--- bet_contract/programs/bet_contract/src/lib.rs
+++ bet_contract/programs/bet_contract/src/lib.rs
@@ -1,4 +1,5 @@
 use anchor_lang::prelude::*;
+use instructions::*;
 mod instructions;
 mod state;
 
@@ -8,11 +9,7 @@ declare_id!("FBzKRcpiLyb6zMM8XVU8mBvJdHxyknGBioihUGFxcuh4");
 pub mod bet_contract {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
+    pub fn initialize(ctx: Context<Initialize>, amount_of_bet_in_sol: u64, rating: u64) -> Result<()> {
+        ctx.accounts.initialize_bet_with_initializer(&ctx.bumps, amount_of_bet_in_sol, rating)
     }
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}
