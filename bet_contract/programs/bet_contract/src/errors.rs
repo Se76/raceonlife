4b44551ccb260ef226935e6788700b0f56fd0382
NickNut <68846529+Se76@users.noreply.github.com>
2025-01-11 14:46:23 +0100

    add two custom errors

---
+++ bet_contract/programs/bet_contract/src/errors.rs
@@ -0,0 +1,9 @@
+use anchor_lang::error_code;
+
+#[error_code]
+pub enum BetContractError {
+    #[msg("The bet has already been taken")]
+    BetAlreadyTaken,
+    #[msg("There is not enough funds")]
+    NotEnoughFunds
+}
