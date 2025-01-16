56e460b4578044ae934382bea33cdf514fa50bb8
NickNut <68846529+Se76@users.noreply.github.com>
2025-01-16 21:39:08 +0100

    add small changes to accounts

--- bet_contract/programs/bet_contract/src/instructions/winner.rs
+++ bet_contract/programs/bet_contract/src/instructions/winner.rs
@@ -1,4 +1,5 @@
 use anchor_lang::prelude::*;
+use crate::state::Config;
 
 #[derive(Accounts)]
 pub struct Winner<'info> {
@@ -7,9 +8,27 @@ pub struct Winner<'info> {
     /// Don't check
     #[account(mut)]
     pub taker: AccountInfo<'info>,
-    /// Don't check
-    #[account(mut)]
-    pub winner: AccountInfo<'info>,
+    // /// Don't check
+    // #[account(mut)]
+    // pub winner: AccountInfo<'info>,
+
+    #[account(
+        seeds = [b"config", initializer.key().as_ref(), b"bet_raceonlife"],
+        bump = config.config_bump,
+    )]
+    pub config: Account<'info, Config>,
+
+    #[account(
+        mut,
+        seeds = [
+                    b"vault", 
+                    initializer.key().as_ref(), 
+                    config.key().as_ref(),
+                    b"bet_raceonlife"
+                ],
+        bump = config.vault_bump,
+    )]
+    pub vault: SystemAccount<'info>,
     // TODO vault, config
     pub system_program: Program<'info, System>
 }
